613e6879a64e19853b8a59a12def68ef0cad6d1f
Stephen Fluin <stephen.fluin@gmail.com>
2022-07-24 20:38:07 -0500

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,33 @@ import { ExpandableItem } from './expandable-item';
 import { RouterModule } from '@angular/router';
 import { CheatSheetComponent } from './cheat-sheet/cheat-sheet.component';
 import { HomeComponent } from './home/home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpandableItem,
     CheatSheetComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav',
+  template: `
+    <mat-sidenav-container class="sidenav-container">
+      <mat-sidenav #drawer class="sidenav" fixedInViewport
+          [attr.role]="(isHandset$ | async) ? 'dialog' : 'navigation'"
+          [mode]="(isHandset$ | async) ? 'over' : 'side'"
+          [opened]="(isHandset$ | async) === false">
+        <mat-toolbar>Menu</mat-toolbar>
+        <mat-nav-list>
+<div><a href="/">Home</a></div>
+<div><a href="/cheatsheet">Switch Seed</a></div>
+        </mat-nav-list>
+      </mat-sidenav>
+      <mat-sidenav-content>
+        <mat-toolbar color="primary">
+          <button
+            type="button"
+            aria-label="Toggle sidenav"
+            mat-icon-button
+            (click)="drawer.toggle()"
+            *ngIf="isHandset$ | async">
+            <mat-icon aria-label="Side nav toggle icon">menu</mat-icon>
+          </button>
+          <span>Minecraft Randomizer Tools</span>
+        </mat-toolbar>
+        <div id="page"><ng-content></ng-content></div>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+    
+  `,
+  styles: [`
+   #page {
+    padding: 16px;
+   }
+    .sidenav-container {
+      height: 100%;
+    }
+    
+    .sidenav {
+      width: 200px;
+    }
+    
+    .sidenav .mat-toolbar {
+      background: inherit;
+    }
+    
+    .mat-toolbar.mat-primary {
+      position: sticky;
+      top: 0;
+      z-index: 1;
+    }
+    
+  `]
+})
+export class NavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+}
